80f72de4f46bf1eddb7ab5eff4fef2d5d333a422
ddavidd <kristiandavidbautista@gmail.com>
2025-10-05 03:46:51 +0800

    image added and randomizer of images every runtime

--- app/src/main/java/com/example/memorygame/GameInterface.kt
+++ app/src/main/java/com/example/memorygame/GameInterface.kt
@@ -6,9 +6,8 @@ import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isGone
 
 class GameInterface : AppCompatActivity() {
 
@@ -16,27 +15,35 @@ class GameInterface : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_interface)
 
+        // list of image resources
+        val images = mutableListOf(
+            R.drawable.lace, R.drawable.hornet, R.drawable.shakra, R.drawable.sherma, R.drawable.garmond, R.drawable.trobbio
+        )
+        images.addAll(images) // duplicate images 6x2 = 12
+        images.shuffle() // shuffles image
+
         val cards = listOf(
-            Pair(findViewById<FrameLayout>(R.id.card1), findViewById<TextView>(R.id.card1Text)),
-            Pair(findViewById<FrameLayout>(R.id.card2), findViewById<TextView>(R.id.card2Text)),
-            Pair(findViewById<FrameLayout>(R.id.card3), findViewById<TextView>(R.id.card3Text)),
-            Pair(findViewById<FrameLayout>(R.id.card4), findViewById<TextView>(R.id.card4Text)),
-            Pair(findViewById<FrameLayout>(R.id.card5), findViewById<TextView>(R.id.card5Text)),
-            Pair(findViewById<FrameLayout>(R.id.card6), findViewById<TextView>(R.id.card6Text)),
-            Pair(findViewById<FrameLayout>(R.id.card7), findViewById<TextView>(R.id.card7Text)),
-            Pair(findViewById<FrameLayout>(R.id.card8), findViewById<TextView>(R.id.card8Text)),
-            Pair(findViewById<FrameLayout>(R.id.card9), findViewById<TextView>(R.id.card9Text)),
-            Pair(findViewById<FrameLayout>(R.id.card10), findViewById<TextView>(R.id.card10Text)),
-            Pair(findViewById<FrameLayout>(R.id.card11), findViewById<TextView>(R.id.card11Text)),
-            Pair(findViewById<FrameLayout>(R.id.card12), findViewById<TextView>(R.id.card12Text))
+            Pair(findViewById<FrameLayout>(R.id.card1), findViewById<ImageView>(R.id.card1Image)),
+            Pair(findViewById<FrameLayout>(R.id.card2), findViewById<ImageView>(R.id.card2Image)),
+            Pair(findViewById<FrameLayout>(R.id.card3), findViewById<ImageView>(R.id.card3Image)),
+            Pair(findViewById<FrameLayout>(R.id.card4), findViewById<ImageView>(R.id.card4Image)),
+            Pair(findViewById<FrameLayout>(R.id.card5), findViewById<ImageView>(R.id.card5Image)),
+            Pair(findViewById<FrameLayout>(R.id.card6), findViewById<ImageView>(R.id.card6Image)),
+            Pair(findViewById<FrameLayout>(R.id.card7), findViewById<ImageView>(R.id.card7Image)),
+            Pair(findViewById<FrameLayout>(R.id.card8), findViewById<ImageView>(R.id.card8Image)),
+            Pair(findViewById<FrameLayout>(R.id.card9), findViewById<ImageView>(R.id.card9Image)),
+            Pair(findViewById<FrameLayout>(R.id.card10), findViewById<ImageView>(R.id.card10Image)),
+            Pair(findViewById<FrameLayout>(R.id.card11), findViewById<ImageView>(R.id.card11Image)),
+            Pair(findViewById<FrameLayout>(R.id.card12), findViewById<ImageView>(R.id.card12Image))
         )
 
-        for ((card, cardText) in cards) {
+        for ((index, cardData) in cards.withIndex()) {
+            val (card, cardImage) = cardData
+            cardImage.setImageResource(images[index])
             card.setOnClickListener {
                 val scale = applicationContext.resources.displayMetrics.density
                 card.cameraDistance = 8000 * scale
 
-                // animations
                 val flipOut = AnimatorInflater.loadAnimator(applicationContext, R.animator.flip_out)
                 val flipIn = AnimatorInflater.loadAnimator(applicationContext, R.animator.flip_in)
 
@@ -46,12 +53,12 @@ class GameInterface : AppCompatActivity() {
                 flipOut.addListener(object : AnimatorListenerAdapter() {
                     override fun onAnimationEnd(animation: Animator) {
                         super.onAnimationEnd(animation)
-                        if (cardText.isGone) {
+                        if (cardImage.visibility == View.GONE) {
                             card.getChildAt(0).visibility = View.GONE
-                            cardText.visibility = View.VISIBLE
+                            cardImage.visibility = View.VISIBLE
                         } else {
                             card.getChildAt(0).visibility = View.VISIBLE
-                            cardText.visibility = View.GONE
+                            cardImage.visibility = View.GONE
                         }
                         flipIn.start()
                     }
